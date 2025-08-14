10cb7d32185954f382bc1577f0ee6594e41a0ccf
Git0104 <us.tan0104@icloud.com>
2025-08-14 20:32:27 +0900

    Update hamburger.js

--- hamburger.js
+++ hamburger.js
@@ -1,17 +1,8 @@
-...
-  <footer>
-    <p>&copy; 2025 株式会社フェニックス</p>
-  </footer>
-
-  <script>
-    function toggleMenu() {
-      const nav = document.querySelector('.nav-buttons');
-      if(nav.style.display === 'flex'){
-        nav.style.display = 'none';
-      } else {
-        nav.style.display = 'flex';
-      }
-    }
-  </script>
-</body>
-</html>
+function toggleMenu() {
+  const nav = document.querySelector('.nav-buttons');
+  if(nav.style.display === 'flex'){
+    nav.style.display = 'none';
+  } else {
+    nav.style.display = 'flex';
+  }
+}
