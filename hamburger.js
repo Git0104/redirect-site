dbe7a224dd1e096dd626a939c7f5f41646d6380d
Git0104 <us.tan0104@icloud.com>
2025-08-14 20:31:22 +0900

    Create hamburger.js

---
+++ hamburger.js
@@ -0,0 +1,17 @@
+...
+  <footer>
+    <p>&copy; 2025 株式会社フェニックス</p>
+  </footer>
+
+  <script>
+    function toggleMenu() {
+      const nav = document.querySelector('.nav-buttons');
+      if(nav.style.display === 'flex'){
+        nav.style.display = 'none';
+      } else {
+        nav.style.display = 'flex';
+      }
+    }
+  </script>
+</body>
+</html>
